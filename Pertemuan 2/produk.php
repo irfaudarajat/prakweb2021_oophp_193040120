f2b6edbd050bfa995431b11f962d3337785af956
irfaudarajat <irfaudarajat@gmail.com>
2021-09-22 02:02:34 +0700

    menyelesaikan video ke 3 pada pertemuan ke 2

--- Pertemuan 2/produk.php
+++ Pertemuan 2/produk.php
@@ -4,11 +4,35 @@
 // Komik
 // game
 class Produk{
+    public $judul = "judul",
+            $penulis = "penulis",
+            $penerbit = "penerbit",
+            $harga = 0;
 
+    public function sayHello(){
+        return "Hello World!";
 
+    }
 }
 
 $produk1 = new Produk();
+$produk->judul = "Naruto";
+var_dump($produk1);
+
+
 $produk2 = new Produk();
+$produk->judul = "Upin ipin";
+$produk2->tambahProperty = "ahahaha";
+var_dump($produk2);
+
+$produk3 = new Produk();
+$produk3->judul = "Uncharted";
+$produk3->Penulis = "Yamete";
+$produk3->penerbit = "irfa";
+$produk3->harga = 10000; 
+
+echo "Komik : $produk3->penulis, $produk3->penerbit";
+echo "<br>";
+echo $produk3->sayHello();
 
 ?>
