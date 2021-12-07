bfd8a746e91efb16b23f8e3f0d3ebee9a5eee436
irfaudarajat <irfaudarajat@gmail.com>
2021-12-07 23:08:43 +0700

    Menyelesaikan video 5 dipertemuan ke 3

--- Pertemuan 3/setter-dan-getter.php
+++ Pertemuan 3/setter-dan-getter.php
@@ -2,14 +2,11 @@
 
 
 class Produk {
-    public $judul,
+    private $judul,
             $penulis, 
-            $penerbit;
-
-    protected $diskon = 0;
-
-    private $harga;
-          
+            $penerbit,
+            $harga,
+            $diskon = 0;
     
     public function __construct( $judul = "judul", $penulis = "penulis", $penerbit = "penerbit", 
         $harga = 0) {
@@ -18,7 +15,47 @@ class Produk {
         $this->penerbit = $penerbit;
         $this->harga = $harga;
      
+    } 
+
+    public function setJudul( $judul ){
+        
+        $this->judul =  $judul;
+
+    }
+ 
+    
+    public function getJudul() {
+        return $this->judul;
     }    
+
+    public function setPenulis ( $penulis ) {
+        $this->penulis = $penulis;
+    }
+
+    public function getPenulis() {
+        return $this->penulis;
+    }
+
+    public function setPenerbit( $penerbit ){
+        $this->penerbit = $penerbit;
+    }
+
+    public function getPenerbit() {
+        return $this->penerbit;
+    }
+
+    public function setDiskon( $diskon ){
+        $this->diskon = $diskon;
+    }
+
+    public function getDiskon(){
+        return $this->diskon;
+    }
+
+
+    public function setHarga($harga){
+        $this->harga = $harga;
+    }
 
     public function getHarga() {
         return $this->harga - ( $this->harga * $this->diskon / 100 );
@@ -65,9 +102,6 @@ class Game extends Produk {
         $harga);
         $this->waktuMain = $waktuMain;
     }
-    public function setDiskon( $diskon ){
-        $this->diskon = $diskon;
-    }
 
     
 
@@ -99,5 +133,10 @@ echo "<hr>";
 
 $produk2->setDiskon(50);
 echo $produk2->getHarga();
+
+echo "<hr>";
+
+$produk1->setPenulis("Irfau Darajat");
+echo $produk1->getPenulis();
 
 ?>
